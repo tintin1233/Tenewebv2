916c851f76a189dc787073ec855cc9ff13cbd241
Jazz Nase <jazzmichaelnase@gmail.com>
2024-11-21 02:33:15 +0800

    if the logo is clicked or the ciduad it will go to #

--- resources/views/components/landing-page/navbar.blade.php
+++ resources/views/components/landing-page/navbar.blade.php
@@ -1,10 +1,14 @@
 <div class="w-full flex justify-center bg-white p-2 sticky top-0 z-50">
     <div class="w-5/6 flex justify-between items-center">
         <div class="flex items-center gap-5">
-            <img src="{{ asset('logo.png') }}" class="h-12 w-12 rounded-full" />
-            <h1 class="font-bold text-3xl text-neutral">
-                Ciudad De Strike
-            </h1>
+            <a href="http://127.0.0.1:8000/#">
+                <img src="{{ asset('logo.png') }}" class="h-12 w-12 rounded-full" />
+            </a>
+            <a href="#">
+                <h1 class="font-bold text-3xl text-neutral">
+                    Ciudad De Strike
+                </h1>
+            </a>
         </div>
 
         <div class="flex items-center gap-5 text-center">
