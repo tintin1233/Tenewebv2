b0be0f794e87071dd2181e5de7fd404cf94ec25f
Jazz Nase <jazzmichaelnase@gmail.com>
2024-11-21 02:26:42 +0800

    scroll down effect is goodshit now

--- resources/views/components/landing-page/navbar.blade.php
+++ resources/views/components/landing-page/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full flex justify-center bg-white p-2">
+<div class="w-full flex justify-center bg-white p-2 sticky top-0 z-50">
     <div class="w-5/6 flex justify-between items-center">
         <div class="flex items-center gap-5">
             <img src="{{ asset('logo.png') }}" class="h-12 w-12 rounded-full" />
@@ -7,19 +7,18 @@
             </h1>
         </div>
 
-        <div class="flex items-center gap-5  text-center">
-            <a href="#" class="hover:font-bold hover:scale-105 duration-700 ">Home</a>
-            <a href="#" class="hover:font-bold hover:scale-105 duration-700 ">About</a>
-            <a href="#" class="hover:font-bold hover:scale-105 duration-700 ">Service</a>
-            <a href="#" class="hover:font-bold hover:scale-105 duration-700 ">Units</a>
+        <div class="flex items-center gap-5 text-center">
+            <a href="#" class="hover:font-bold hover:scale-105 duration-700">Home</a>
+            <a href="#" class="hover:font-bold hover:scale-105 duration-700">About</a>
+            <a href="#" class="hover:font-bold hover:scale-105 duration-700">Service</a>
+            <a href="#" class="hover:font-bold hover:scale-105 duration-700">Units</a>
 
             @if (!Auth::user())
                 <button href="{{ route('login') }}" class="btn btn-accent btn-sm"
-                    onclick="my_modal_1.showModal()">Login</a>
-                @else
-                    <a href="{{ route('home') }}" class="btn btn-accent btn-sm">Dashboard</a>
+                    onclick="my_modal_1.showModal()">Login</button>
+            @else
+                <a href="{{ route('home') }}" class="btn btn-accent btn-sm">Dashboard</a>
             @endif
-
         </div>
     </div>
 
@@ -34,7 +33,6 @@
             </div>
             <p class="py-4 text-xs">Sign-in your account</p>
 
-
             <form action="{{route('login')}}" method="post" class="flex flex-col gap-2">
                 @csrf
                 <div class="flex flex-col gap-2">
@@ -47,7 +45,6 @@
                 </div>
                 <button class="btn btn-accent">Login</button>
             </form>
-
         </div>
     </dialog>
 </div>
