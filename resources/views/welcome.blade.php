df57902245b6c437b9daec72b6471deb819d0454
crackdetective <165134279+crackdetective@users.noreply.github.com>
2024-11-24 10:58:53 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
 
         <div class="grow flex flex-col justify-center items-center">
             <div class="w-5/6 flex flex-col gap-2">
-                <h1 class="font-bold text-4xl capitalize">Welcome to CIUDAD De Strike HOAI</h1>
+                <h1 class="font-bold text-4xl capitalize">Welcome to CIUDAD De Strike HOAI!</h1>
                 <p class="whitespace-pre-line text-xs">CIUDAD De Strike is a community that values safety, quality of
                     life, and trust.
                     Our neighborhood is designed to provide a comfortable and safe place for you and
