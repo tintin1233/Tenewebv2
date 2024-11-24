1727be272c54839814f8dc35709ea6aeac912c25
crackdetective <165134279+crackdetective@users.noreply.github.com>
2024-11-24 16:45:08 +0800

    Update show.blade.php

--- resources/views/user/tenant/profile/show.blade.php
+++ resources/views/user/tenant/profile/show.blade.php
@@ -1,3 +1,7 @@
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 <x-dashboard.tenant.base>
 
 
