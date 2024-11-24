50dc5d6ad097578adc0b0ea8998db27a8d2bed8d
crackdetective <165134279+crackdetective@users.noreply.github.com>
2024-11-24 14:13:14 +0800

    Update sidebar.blade.php

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -18,6 +18,7 @@
     })->first();
 
 @endphp
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 /* Sidebar Styles */
 .sidebar {
