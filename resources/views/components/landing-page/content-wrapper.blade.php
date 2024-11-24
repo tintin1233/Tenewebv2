77ff39dcc2dc30a86671bec72abe66ad8357d883
crackdetective <165134279+crackdetective@users.noreply.github.com>
2024-11-24 14:32:10 +0800

    test

--- resources/views/components/landing-page/content-wrapper.blade.php
+++ resources/views/components/landing-page/content-wrapper.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full h-auto flex justify-center">
-    <div class="w-5/6 min-h-screen flex flex-col gap-2" style="margin-left:30vh !important;">
+    <div class="w-5/6 min-h-screen flex flex-col gap-2">
         {{$slot}}
     </div>
 </div>
