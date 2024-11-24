728a142566ebeba82688def6c2898db5d8b5130e
crackdetective <165134279+crackdetective@users.noreply.github.com>
2024-11-24 16:59:08 +0800

    Update index.blade.php

--- resources/views/user/tenant/document/index.blade.php
+++ resources/views/user/tenant/document/index.blade.php
@@ -26,7 +26,7 @@
             <a href="{{route('tenant.documents.agreement')}}" class="flex items-center shadow-xl p-5 gap-5 hover:scale-105 duration-700 rounded-lg">
 
                 <i class="fi fi-rr-document-signed text-primary text-5xl hover:text-secondary duration-700"></i>
-                <h1 class="text-xl font-bold text-primary" style="font-size:2vh;">
+                <h1 class="text-xl font-bold text-primary">
                    Agreement
                 </h1>
             </a>
@@ -37,7 +37,7 @@
              <a class="flex items-center shadow-xl p-5 gap-5 hover:scale-105 duration-700 rounded-lg">
 
                 <i class="fi fi-rr-folder-open text-primary text-5xl hover:text-secondary duration-700"></i>
-                <h1 class="text-xl font-bold text-primary" style="font-size:2vh;">
+                <h1 class="text-xl font-bold text-primary">
                     Tenants
                 </h1>
             </a> 
@@ -46,7 +46,7 @@
             <a href="{{route('tenant.documents.penalty')}}" class="flex items-center shadow-xl p-5 gap-5 hover:scale-105 duration-700 rounded-lg">
 
                 <i class="fi fi-rr-document-signed text-primary text-5xl hover:text-secondary duration-700"></i>
-                <h1 class="text-xl font-bold text-primary" style="font-size:2vh;">
+                <h1 class="text-xl font-bold text-primary">
                   Penalties
                 </h1>
             </a>
@@ -55,7 +55,7 @@
             <a href="{{route('tenant.documents.requirement')}}" class="flex items-center shadow-xl p-5 gap-5 hover:scale-105 duration-700 rounded-lg">
 
                 <i class="fi fi-rr-document-signed text-primary text-5xl hover:text-secondary duration-700"></i>
-                <h1 class="text-xl font-bold text-primary" style="font-size:2vh;">
+                <h1 class="text-xl font-bold text-primary">
                     Requirements
                 </h1>
             </a>
